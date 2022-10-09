e8803af83fd63a1a305c66895828eb1af3db9bca
Zhengkun Du <dododugan@gmail.com>
2022-10-09 04:20:46 -0400

    Create 4 folders

---
+++ src/test/java/com/flag/robot_dispatch/RobotDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flag.robot_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotDispatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
