6e731336b9a5ac463d66505a71cb9434ae111083
sawanghan06 <sawanghan06@gmail.com>
2022-10-10 22:04:12 -0500

    test 4.0

--- src/main/java/com/flag/robot_dispatch/model/DispatchCenter.java
+++ src/main/java/com/flag/robot_dispatch/model/DispatchCenter.java
@@ -4,4 +4,10 @@ public class DispatchCenter {
     private String address;
     private double lat;
     private double lon;
+
+    public DispatchCenter(String address, double lat, double lon) {
+        this.address = address;
+        this.lat = lat;
+        this.lon = lon;
+    }
 }
