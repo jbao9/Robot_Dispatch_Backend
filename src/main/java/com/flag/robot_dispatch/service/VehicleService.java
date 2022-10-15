6a380e0a772baa802ce1865b88a71f09da6a40e1
Aaron Fu <ye.fu@wustl.edu>
2022-10-15 02:23:16 -0700

    Add files via upload

---
+++ src/main/java/com/flag/robot_dispatch/service/VehicleService.java
@@ -0,0 +1,50 @@
+package com.flag.robot_dispatch.service;
+
+import com.flag.robot_dispatch.exception.VehicleAlreadyExistException;
+import com.flag.robot_dispatch.exception.VehicleNotExistException;
+import com.flag.robot_dispatch.model.Vehicle;
+import com.flag.robot_dispatch.repository.VehicleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    public Vehicle addVehicle (Vehicle vehicle) {
+        if(canAddVehicle(vehicle)){
+            return vehicleRepository.save(vehicle);
+        } else {
+            throw new VehicleAlreadyExistException("Vehicle Already Exist");
+        }
+    }
+
+    public void deleteVehicle(Long id) {
+        try {
+            vehicleRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new VehicleNotExistException("Vehicle Does Not Exist");
+        }
+    }
+
+    public Vehicle updateVehicle(Vehicle vehicle){
+        Vehicle prevVehicle = vehicleRepository.getReferenceById(vehicle.getId());
+        if(prevVehicle != null){
+            return vehicleRepository.save(vehicle);
+        } else {
+            throw new VehicleNotExistException("No Vehicle Found for Update");
+        }
+    }
+
+    private boolean canAddVehicle(Vehicle vehicle){
+        return vehicleRepository.getReferenceById(vehicle.getId()) != null;
+    }
+}
