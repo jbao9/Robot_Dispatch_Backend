1309f26880bd0af56e7f6d040454df5f5abf0fc2
Zhengkun Du <dododugan@gmail.com>
2022-10-16 14:32:16 -0400

    Add VehicleController (Not Done yet)

---
+++ src/main/java/com/flag/robot_dispatch/controller/VehicleController.java
@@ -0,0 +1,38 @@
+package com.flag.robot_dispatch.controller;
+
+import com.flag.robot_dispatch.model.*;
+import com.flag.robot_dispatch.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// TODO: Not Done Yet.
+
+@RestController
+public class VehicleController {
+    private VehicleService vehicleService;
+
+
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+
+    @PostMapping(value = "/vehicles")
+    public void addVehicle(
+            @RequestParam("name") String name,
+            @RequestParam("status") Status status,
+            @RequestParam("center_id") DispatchCenter center_id,
+            @RequestParam("machine_type") VehicleType machine_type
+    ) {
+        Vehicle vehicle = new Vehicle.Builder()
+                .setName(name).setStatus(status).setType(machine_type).setLocation(center_id).build();
+        vehicleService.addVehicle(vehicle);
+    }
+
+
+// TODO: add get and delete and update methods
+//    ! This is P0 features.
+}
