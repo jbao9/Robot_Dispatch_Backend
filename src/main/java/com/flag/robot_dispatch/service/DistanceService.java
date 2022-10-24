2863245414fbc178393790f288f4bec7fffff61a
sawanghan06 <sawanghan06@gmail.com>
2022-10-23 19:17:48 -0500

    HW 2022/10/23 - 3

--- src/main/java/com/flag/robot_dispatch/service/DistanceService.java
+++ src/main/java/com/flag/robot_dispatch/service/DistanceService.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
 @Service
 public class DistanceService {
     public DistanceService() {
@@ -18,13 +19,33 @@ public class DistanceService {
                 + getDistance(delivery, vehicleLocation);
     }
 
-    /** distance = sqrt((x1 - x2)^2 + (y1 - y2)^2 */
     public double getDistance(Location a, Location b) {
         GeoPoint pointA = a.getGeoPoint();
         GeoPoint pointB = b.getGeoPoint();
-        return Math.sqrt(
-                Math.pow((pointA.getLat() - pointB.getLat()), 2) +
-                        Math.pow((pointA.getLon() - pointB.getLon()), 2)
-        );
+
+        // converts from degrees to radians
+        double lon1 = Math.toRadians(pointA.getLon());
+        double lon2 = Math.toRadians(pointB.getLon());
+        double lat1 = Math.toRadians(pointA.getLat());
+        double lat2 = Math.toRadians(pointA.getLat());
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double x = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2), 2);
+
+        double c = 2 * Math.asin(Math.sqrt(x));
+
+        // Radius of earth:
+        // 6371 in kilometers/3956 in miles
+        double r = 3956;
+
+        // calculate the result
+        return (c * r);
     }
+
 }
+
+
